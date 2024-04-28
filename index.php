dcc3ccfc1e12d5b0566deda358890fc13fd8d8a6
Cyril Roussillon <cyril42e@gmail.com>
2024-04-28 12:11:09 +0200

    make it standalone for hosting
    (was on jsfiddle previously)

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
-<pre></pre>
-<!--<ul id="lines"></ul> -->
-<body>
-<div id="lists"></div>
-</body>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Prochain train</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+</head>
 
+<body>
+  <div id="lists"></div>
+
+  <script src="script.js"></script>
+</body>
+</html>
